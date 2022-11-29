80453d0148f703d62428140036f1a8fd3707474d
PrangonGhose <prangonghose@gmail.com>
2022-11-29 23:52:02 +0600

    Fix some issues with JavaScript

--- script.js
+++ script.js
@@ -8,19 +8,18 @@ const bookList = document.querySelector('.book-list');
 const titleValue = document.querySelector('.form-book-title');
 const authorValue = document.querySelector('.form-author-name');
 const form = document.querySelector('form');
-const keys = [];
 
 /* Declare a function to add books in HTML */
 
 function addBooks(index) {
   const article = document.createElement('article');
   article.classList.add('books');
-  const id = `article${index}`;
+  const id = `article${bookArray[index].bookID}`;
   article.setAttribute('id', id);
   article.innerHTML = `  
   <h4 class="book-name">${bookArray[index].bookTitle}</h4>
   <p class="author-name">${bookArray[index].authorName}</p>
-  <button class="remove-button" id="${index}" onclick="removeButtons(id)">Remove</button>
+  <button class="remove-button" id="${bookArray[index].bookID}" onclick="removeButtons(id)">Remove</button>
   <hr>`;
   bookList.appendChild(article);
 }
@@ -31,18 +30,24 @@ function removeButtons(id) {
   const articleID = `#article${id}`;
   const removeArticle = document.querySelector(articleID);
   bookList.removeChild(removeArticle);
-  window.localStorage.removeItem(keys[id]);
+  storedDataArray = storedDataArray.filter((item) => item.bookID != id)
+  dataStore();
+  if (storedDataArray.length === 0) {
+    window.localStorage.removeItem('name');
+  }
 }
 
 let i = 0;
 
 /* Declare a function to create bookObject and push it to bookArray */
 
-function add(bookTitle, authorName) {
+function add(bookID, bookTitle, authorName) {
   const bookObject = {
+    bookID: null,
     bookTitle: '',
     authorName: '',
   };
+  bookObject.bookID = bookID;
   bookObject.bookTitle = bookTitle;
   bookObject.authorName = authorName;
   bookArray.push(bookObject);
@@ -52,40 +57,38 @@ function add(bookTitle, authorName) {
 
 /* Using local storage */
 
-const storedDataArray = [];
+let storedDataArray = [];
+let b_id = 0;
 
-function dataStore(index) {
-  const name = `name ${index}`;
-  localStorage.setItem(name, JSON.stringify(storedDataArray[index]));
-  keys.push(name);
+function dataStore() {
+  localStorage.setItem('name', JSON.stringify(storedDataArray));
 }
-
-let position = 0;
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const storedData = {
+    bookID: null,
     title: '',
     author: '',
   };
+  storedData.bookID = b_id;
   storedData.title = titleValue.value;
   storedData.author = authorValue.value;
   storedDataArray.push(storedData);
-  dataStore(position);
-  add(storedData.title, storedData.author);
-  position += 1;
+  dataStore();
+  add(storedData.bookID, storedData.title, storedData.author);
   titleValue.value = null;
   authorValue.value = null;
+  b_id += 1;
 });
 
-if (JSON.parse(localStorage !== null)) {
-  for (let x = 0; x < localStorage.length; x += 1) {
-    const storedData = JSON.parse(localStorage.getItem(localStorage.key(x)));
-    if (storedData !== null) {
-      storedDataArray.push(storedData);
-      keys.push(localStorage.key(x));
-      add(storedData.title, storedData.author);
-      position += 1;
+if (JSON.parse(localStorage.getItem('name') !== null)) {
+  storedDataArray = JSON.parse(localStorage.getItem('name'));
+  b_id = storedDataArray[storedDataArray.length - 1].bookID + 1;
+  console.log(b_id);
+  if (storedDataArray !== null) {
+    for (let x = 0; x < storedDataArray.length; x += 1) {
+      add(storedDataArray[x].bookID, storedDataArray[x].title, storedDataArray[x].author);
     }
   }
 }
